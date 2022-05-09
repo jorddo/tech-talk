b3c1fde841310f4a5677b27b224d60d914e1bbf8
jorddo <jordanbsneed@gmail.com>
2022-05-08 19:31:28 -0500

    removing tags, reqs, and deadlines

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,34 +1,28 @@
 async function newPost(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('#post-title').value.trim();
-    const body = document.querySelector('#post-body').value.trim();
-    const deadline = document.querySelector('#post-deadline').value.trim();
-    const requirements = document.querySelector('#post-requirements').value.trim();
-    const tag = document.querySelector('#tag').value;
+  const title = document.querySelector('#post-title').value.trim();
+  const body = document.querySelector('#post-body').value.trim();
 
-    if (title && body && tag) {
-        const response = await fetch(`/helpPost`, {
-            method: 'post',
-            body: JSON.stringify({
-                title,
-                body,
-                deadline,
-                tag,
-                requirements
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                const id = data.post.id
-                document.location.replace(`/helppost/${id}`)
-            })
-    } else {
-        alert('Please fill out the appropriate fields')
-    };
-};
+  if (title && body) {
+    const response = await fetch(`/helpPost`, {
+      method: 'post',
+      body: JSON.stringify({
+        title,
+        body,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const id = data.post.id;
+        document.location.replace(`/helppost/${id}`);
+      });
+  } else {
+    alert('Please fill out the appropriate fields');
+  }
+}
 // link to post form
 document.querySelector('#add-post-btn').addEventListener('click', newPost);
--- public/javascript/postTags.js
+++
@@ -1,6 +0,0 @@
-function postByTag(event) {
-    event.preventDefault();
-    const tag = document.querySelector('#tag-options').value;
-    document.location.assign(`/dashboard/${tag}`)
-}
-document.querySelector('#find-post-btn').addEventListener('click', postByTag);
